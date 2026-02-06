1ac3d90ea76a1b463fb730c9914d7cd07b2fed86
Sam Rizk194 <samehrizk1993@gmail.com>
2026-02-06 17:36:14 +0200

    adding elements to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import assets from "../assets/assets";
 
 function Navbar({ theme, setTheme }) {
+  const [sidebarOpen, setsidebarOPen] = useState(false);
   return (
     <div
       className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 
@@ -13,26 +15,71 @@ function Navbar({ theme, setTheme }) {
       />
 
       <div
-        className="text-gray-700 dark:text-white sm:text-sm max-sm:w-60
-       max-sm:pl-10 max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen
+        className={`text-gray-700 dark:text-white sm:text-sm ${
+          !sidebarOpen
+            ? "max-sm:w-0 overflow-hidden"
+            : "max-sm:w-60 max-sm:pl-10"
+        } max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen
         max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white 
-        max-sm:bt-20 flex sm:items-center gap-5 transition-all"
+        max-sm:pt-20 flex sm:items-center gap-5 transition-all`}
       >
-        <a href="#" className="sm:hover:border-b">
+        <img
+          src={assets.close_icon}
+          alt=""
+          className="w-5 absolute top-4 right-4 sm:hidden"
+          onClick={() => {
+            setsidebarOPen(false);
+          }}
+        />
+
+        <a
+          onClick={() => {
+            setsidebarOPen(false);
+          }}
+          href="#"
+          className="sm:hover:border-b"
+        >
           Home
         </a>
-        <a href="#services" className="sm:hover:border-b">
+        <a
+          onClick={() => {
+            setsidebarOPen(false);
+          }}
+          href="#services"
+          className="sm:hover:border-b"
+        >
           Services
         </a>
-        <a href="#our-work" className="sm:hover:border-b">
+        <a
+          onClick={() => {
+            setsidebarOPen(false);
+          }}
+          href="#our-work"
+          className="sm:hover:border-b"
+        >
           Our Work
         </a>
-        <a href="#contact-us" className="sm:hover:border-b">
+        <a
+          onClick={() => {
+            setsidebarOPen(false);
+          }}
+          href="#contact-us"
+          className="sm:hover:border-b"
+        >
           Contact Us
         </a>
       </div>
 
-      <div>
+      <div className="flex items-center gap-2 sm:gap-4">
+        <img
+          src={theme === "dark" ? assets.menu_icon_dark : assets.menu_icon}
+          alt=""
+          onClick={() => {
+            setsidebarOPen(true);
+          }}
+          className="w-8 sm:hidden"
+        />
+
         <a
           href="contact-us"
           className="text-sm max-sm:hidden flex items-center gap-2
