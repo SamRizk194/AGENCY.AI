2e9408add61a1999865c88b2fd90ae031c5c4ff4
Sam Rizk194 <samehrizk1993@gmail.com>
2026-02-08 00:42:30 +0200

    continue hero sec

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,17 @@ import assets from "../assets/assets";
 import ThemeToggleBtn from "./ThemeToggleBtn";
 
 function Navbar({ theme, setTheme }) {
-  const [sidebarOpen, setsidebarOPen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div
       className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 
-    py-4 sticky top-0 z-20 backdrop:blur-xl font-medium pg-white/50 dark:bg-gray-900/70"
+      py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70"
     >
       <img
         src={theme === "dark" ? assets.logo_dark : assets.logo}
         className="w-32 sm:w-40"
-        alt=""
+        alt="Logo"
       />
 
       <div
@@ -26,45 +27,35 @@ function Navbar({ theme, setTheme }) {
       >
         <img
           src={assets.close_icon}
-          alt=""
+          alt="Close menu"
           className="w-5 absolute top-4 right-4 sm:hidden"
-          onClick={() => {
-            setsidebarOPen(false);
-          }}
+          onClick={() => setSidebarOpen(false)}
         />
 
         <a
-          onClick={() => {
-            setsidebarOPen(false);
-          }}
-          href="#"
+          href="#hero"
+          onClick={() => setSidebarOpen(false)}
           className="sm:hover:border-b"
         >
           Home
         </a>
         <a
-          onClick={() => {
-            setsidebarOPen(false);
-          }}
           href="#services"
+          onClick={() => setSidebarOpen(false)}
           className="sm:hover:border-b"
         >
           Services
         </a>
         <a
-          onClick={() => {
-            setsidebarOPen(false);
-          }}
           href="#our-work"
+          onClick={() => setSidebarOpen(false)}
           className="sm:hover:border-b"
         >
           Our Work
         </a>
         <a
-          onClick={() => {
-            setsidebarOPen(false);
-          }}
           href="#contact-us"
+          onClick={() => setSidebarOpen(false)}
           className="sm:hover:border-b"
         >
           Contact Us
@@ -76,20 +67,18 @@ function Navbar({ theme, setTheme }) {
 
         <img
           src={theme === "dark" ? assets.menu_icon_dark : assets.menu_icon}
-          alt=""
-          onClick={() => {
-            setsidebarOPen(true);
-          }}
-          className="w-8 sm:hidden"
+          alt="Open menu"
+          onClick={() => setSidebarOpen(true)}
+          className="w-8 sm:hidden cursor-pointer"
         />
 
         <a
-          href="contact-us"
+          href="#contact-us"
           className="text-sm max-sm:hidden flex items-center gap-2
            bg-primary text-white px-6 py-2 rounded-full cursor-pointer
-            hover:scale-103 transition-all "
+            hover:scale-105 transition-all"
         >
-          Connect <img src={assets.arrow_icon} width={14} alt="" />
+          Connect <img src={assets.arrow_icon} width={14} alt="Arrow icon" />
         </a>
       </div>
     </div>
